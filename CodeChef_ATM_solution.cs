819b9e1bdb759f798a45094ab142c2912b654c5c
jc566 <jc566@njit.edu>
2017-10-17 09:38:17 -0400

    Added C# solution for ATM problem

---
+++ CodeChef_ATM_solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+/*
+
+CodeChef URL link = https://www.codechef.com/problems/HS08TEST
+Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.
+
+Input
+Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to withdraw.
+
+Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's initial account balance.
+
+Output
+Output the account balance after the attempted transaction, given as a number with two digits of precision. If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+Example - Successful Transaction
+Input:
+30 120.00
+
+Output:
+89.50
+Example - Incorrect Withdrawal Amount (not multiple of 5)
+Input:
+42 120.00
+
+Output:
+120.00
+Example - Insufficient Funds
+Input:
+300 120.00
+
+Output:
+120.00
+*/
+
+    class CodeChef_ATM_solution
+{
+        static void Main(string[] args)
+        {
+            //take in the user input and parse it
+            string[] userInput = Console.ReadLine().Split(' ');
+
+            float withdrawalAmount = float.Parse(userInput[0]);
+            float currentBalance = float.Parse(userInput[1]);
+
+            if(withdrawalAmount % 5 == 0 && withdrawalAmount <= currentBalance - 0.5f)
+            {
+                Console.WriteLine("{0:f2}", (currentBalance - withdrawalAmount - 0.50));
+            }
+            else if(withdrawalAmount % 5 == 0 && withdrawalAmount > currentBalance)
+            {
+                Console.WriteLine(currentBalance.ToString("0.00"));
+            }
+            else
+            {
+                //can use c2 as well in ToString()
+                Console.WriteLine((currentBalance).ToString("0.00"));
+            }
+        }
+            
+    }
+
